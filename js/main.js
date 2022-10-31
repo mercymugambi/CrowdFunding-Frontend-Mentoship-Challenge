8e64ef8bf59bf6a89e9462607a6c6205c48270f7
mercy <mercymugambi2016@gmail.com>
2022-10-31 20:51:35 +0300

    css changes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const navToggle = document.querySelector('#navToggle');
 const nav = document.querySelector('nav');
 const navIcon = document.querySelectorAll('.navIcon');
 const hamburger = document.querySelector('#hamburger');
+
 navToggle.addEventListener("click", () => {
     nav.classList.toggle('open');
     navIcon.forEach(icon => {
